d03bab35aa7db36e419692fd473580eac819a69c
suregirishkumarreddy <suregirishkumarreddy@gmail.com>
2021-01-15 20:26:29 -0600

    Create Assignment-13 - Classes & Objects (Shirt).py

---
+++ Assignment-13 - Classes & Objects (Shirt).py
@@ -0,0 +1,24 @@
+class shirt_details:
+
+    def __init__(self, brand, model, color, size, price):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.size = size
+        self.price = price
+
+    def getDetails(self):
+        print("Brand : " + self.brand)
+        print("Model : " + self.model)
+        print("Color : " + self.color)
+        print("Size : " + self.size)
+        print("Price : " + self.price)
+
+# Creating an instance of Shirt
+shirt_lee = shirt_details("Lee", "Button Down", "Blue", "Large", "29.99")
+shirt_levis = shirt_details("Levis", "T-Shirt", "Green", "X-Large", "24.99")
+
+
+# Print the details of Shirt added
+shirt_lee.getDetails()
+shirt_levis.getDetails()
